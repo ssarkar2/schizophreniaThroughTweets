5f8b825893049cd2e536926dd2912afeeb4c6310
ssarkar2 <supersarkar@gmail.com>
2016-04-12 20:16:03 -0400

    Correct reading of single tweet
    Also a function to read all files in a folder

--- utilities/tweetReading/readTweet.py
+++ utilities/tweetReading/readTweet.py
@@ -1,14 +1,27 @@
 import os, gzip, json
 
-def readTweet(inpFile):  #can read both gz and extracted tweets
-    if (inpFile.split('.')[-1] == 'gz'):
-        return json.loads(gzip.open(inpFile, 'rb').readline())
-    else:
-        return json.loads(open(inpFile, 'rb').readline())
+def getIDFromFilename(filename):  #get unique id from file name
+    return filename.split('\\')[-1].split('.')[0]
 
-a = readTweet('C:\Sayantan\\acads\cmsc773\proj\data\data\clpsych2015\schizophrenia\\anonymized_control_tweets\\7VbnLI9dA.txt')
-b = readTweet('C:\Sayantan\\acads\cmsc773\proj\data\data\clpsych2015\schizophrenia\\anonymized_control_tweets\\7VbnLI9dA.tweets.gz')
-print a
-print
-print
-print b
+def readTweet(inpFile):  #can read both gz and extracted tweets
+    openfunc = (open, gzip.open)[inpFile.split('.')[-1] == 'gz']  #choose a open function based on if its compressed or not
+    return (getIDFromFilename(inpFile), [json.loads(i) for i in openfunc(inpFile, 'rb').read().split('\n') if len(i) > 0])  #return a tuple (id, list of dictionaries). each dictionary is a tweet
+
+
+def getAllTweets(inpFolder):
+    return [readTweet(inpFolder+filename) for filename in os.listdir(inpFolder)]
+
+
+
+#read single tweet
+a = readTweet('C:\Sayantan\\acads\cmsc773\proj\data\data\clpsych2015\schizophrenia\\bebChK7PskxB.txt')
+print 'done'
+b = readTweet('C:\Sayantan\\acads\cmsc773\proj\data\data\clpsych2015\schizophrenia\\anonymized_control_tweets\\bebChK7PskxB.tweets.gz')
+##print a
+##print
+##print
+##print b
+
+alltweets = getAllTweets('C:\Sayantan\\acads\cmsc773\proj\data\data\clpsych2015\schizophrenia\\anonymized_control_tweets\\')
+print len(alltweets)
+print alltweets[0][0]
--- utilities/unzipFiles/unzipAll.py
+++ utilities/unzipFiles/unzipAll.py
@@ -6,12 +6,12 @@ def createDir(dirname): #check if such a directory exists, else create a new one
 
 def unzipAll(sourceDir, targetDir):
     createDir(targetDir)
-    for filename in os.listdir(sourceDir):     
+    for filename in os.listdir(sourceDir):
         fout = open(targetDir + filename.split('.')[0] + '.txt', 'w')
-        f = gzip.open(sourceDir + filename, 'rb')        
-        fout.write(f.readline())
-        
-    
+        f = gzip.open(sourceDir + filename, 'rb')
+        fout.write(f.read())
+
+
 #note directories should end with '/' in the input arguments
 #note extraction is not really required, you can just read off the gz files directly as is done in lines 11-12
 unzipAll('/scratch0/sem4/cmsc773/data/data/clpsych2015/schizophrenia/anonymized_control_tweets/', '/scratch0/sem4/cmsc773/data/data/clpsych2015/schizophrenia/anonymized_control_tweets_unzipped/')
