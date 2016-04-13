f342e401ac69df53b31e38343ebc15d2cf1a26b9
ssarkar2 <supersarkar@gmail.com>
2016-04-12 23:46:52 -0400

    Add number filters (age, num_tweets)
    Note that the ranges required in the number filters are inclusive

--- utilities/tweetReading/readTweet.py
+++ utilities/tweetReading/readTweet.py
@@ -11,14 +11,14 @@ def readTweet(inpFile):  #can read both gz and extracted tweets
 def getAllTweets(inpFolder):  #return all tweets from all users in a directory. takes time to run
     return [readTweet(inpFolder+filename) for filename in os.listdir(inpFolder)]
 
-def readCSV(filename, categoryFilter = None):
+def readCSV(filename, categoryFilter = None, numFilter = None):
     info = []
     fcsv = open(filename, 'r')
     for row in fcsv.read().split('\n')[1:]:  #dropping the first row as it contains col names
         elements = row.split(',')
         if len(elements) == 6:
             entry = {'anonymized_name':elements[0], 'condition':elements[1], 'age':float(elements[2]), 'gender':elements[3], 'num_tweets':int(elements[4]), 'fold':int(elements[5])}
-            if checkCategoryFilter(entry, categoryFilter):
+            if checkCategoryFilter(entry, categoryFilter) and checkNumberFilter(entry, numFilter):
                 info.append(entry)
     fcsv.close()
     return info
@@ -28,15 +28,25 @@ def checkCategoryFilter(entry, categoryFilter): # categoryFilter is a dictionary
         return True
     else:
         for i in categoryFilter:
-            #print i, entry[i], categoryFilter[i], entry[i] in categoryFilter[i], entry[i] in categoryFilter[i] == False
             if not(entry[i] in categoryFilter[i]):
                 return False #even if one fails, return false
-##            if i == 'fold':
-##                return entry[i] in categoryFilter[i]
-##            else:
-##                if entry[i] != categoryFilter[i]:
-##                    return False  #even if one fails, return false
         return True  #all checks passed. return True
+
+def checkNumberFilter(entry, numFilter):
+    if numFilter == None:  #no filter present, so append all entries to return list
+        return True
+    else:
+        for i in numFilter:
+            retVal = False
+            for numRange in numFilter[i]:
+                if entry[i]>=numRange[0] and entry[i]<=numRange[1]:
+                    retVal = True
+            if retVal == False:
+                return False
+        return True
+
+
+
 
 
 #read single tweet
@@ -56,4 +66,5 @@ def checkCategoryFilter(entry, categoryFilter): # categoryFilter is a dictionary
 
 print readCSV('C:\Sayantan\\acads\cmsc773\proj\data\data\clpsych2015\schizophrenia\\anonymized_user_manifest.csv')
 print; print
-print readCSV('C:\Sayantan\\acads\cmsc773\proj\data\data\clpsych2015\schizophrenia\\anonymized_user_manifest.csv', {'condition':'control', 'gender':'M', 'fold':[1,2]})
+x = readCSV('C:\Sayantan\\acads\cmsc773\proj\data\data\clpsych2015\schizophrenia\\anonymized_user_manifest.csv', {'condition':'control', 'gender':'M', 'fold':[1,2]}, {'age':[(20,22), (23,25)], 'num_tweets':[(1,1000),(2000,5000)]})
+print x
