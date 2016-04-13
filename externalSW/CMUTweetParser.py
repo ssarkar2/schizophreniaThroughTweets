325dbfcb990742c20bd327bd9eb004c574449756
ssarkar2 <supersarkar@gmail.com>
2016-04-13 13:17:49 -0400

    Completed CMURunTaggerSingleTweetFile

--- externalSW/CMUTweetParser.py
+++ externalSW/CMUTweetParser.py
@@ -3,14 +3,17 @@
 from utilities.readTweet import *
 import os
 
-CMUParserLoc = '/scratch0/sem4/cmsc773/code/ark-tweet-nlp-0.3.2'
+CMUParserLoc = '/scratch0/sem4/cmsc773/code/ark-tweet-nlp-0.3.2/'
 
 def CMURunTaggerSingleTweetFile(tweetFile, num = None, options = ''):  #1st num number of tweets are parsed. If its None, all are parsed
     t = readTweetsFromFile(tweetFile)
     f = open('mytemptext.txt', 'w')
     for tweet in t:  #tweet is a dictionary representing a tweet
-        f.write(tweet['text']+'\n')   #note if 'text' does not exist it will throw an error. handle that if it occurs. should not happen though
+        f.write(tweet['text'].encode('ascii','ignore') +'\n')   #note if 'text' does not exist it will throw an error. handle that if it occurs. should not happen though
+        #note special unicode characters are ignored. replaced by ?
     f.close()
+    os.system(CMUParserLoc + 'runTagger.sh ' + options + ' mytemptext.txt')
+    os.remove('mytemptext.txt')
 
 
 
