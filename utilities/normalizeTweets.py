3109ba6f393dc47382da60905d4fc95aa553dc3c
ssarkar2 <supersarkar@gmail.com>
2016-04-22 18:17:47 -0400

    Added hashtag separator
    1) Core hashtag separator is in wordSeparator file
    2) Moved annotated emoticons
    3) norTweet1 function has been beefed up. Please read detailed description of what it does in normalizeTweets file
    4) See how to use normTweet1 in demoNormalizeTweet

--- utilities/normalizeTweets.py
+++ utilities/normalizeTweets.py
@@ -1,19 +1,63 @@
 from utilities.ark_tweet_nlp_0_3_2.ark_tweet_nlp_python.CMUTweetTagger import *
 from utilities.spellChecker import spellCorrectTokenizedTweets
+from utilities.wordSeparator import parseTagSingleWord
 import pickle
 import os
 
-def normTweet1(tweets, ops = [0,1]):
+'''
+description of normTweet1 usage
+normTweet1 takes tweetList as input...which is a list of tweet strings
+It tokenizes them using tokenizeCMUPython
+
+getSpecialTokens is called next. it divides the tokens in the tweets by token types
+eg: from tokenizer we got: [[('#helloBye', '#', 0.9), ('there', 'D', 0.9)], [('an', 'D', 0.9), (':)', 'E', 0.9), ('#spelingMistake', '#', 0.9)], [('Wrld', 'N', 0.9)]]
+Then getSpecialTokens(tokenizedTweets, f, ops, ['#', 'E'], retain = 0) will return:
+separatedByTokenType = [[hashtags] , [emoticons] , [rest of the things without hashtags or emoticons]]   Note if retain was 1, then the 3rd sublist would have retained the hashtags and emoticons
+expanding separatedByTokenType explicitly, it looks like: [[[('hello bye', '#', 0.765)], [('spe ling mistake', '#', 0.8214)], []], [[], [(':)', 'E', 0.9852)], []], [[('there', 'R', 0.5307)], [('an', 'D', 0.7642)], [('Wrld', 'N', 0.3949)]]]
+So what happens here is that the words in the hashtags are separated out and placed in the first sublist (per tweet basis).Then emoticons (per tweet basis) are placed in second bin and the rest of the stuff in the 3rd bin. Note if there are no # in a tweet, then that entry of the 1st bin is empty
+eg separatedByTokenType[0][2] is empty, as the tweet#2 does not have any hashtags
+
+Finally spellcheck and clean up is performed on separatedByTokenType[-1]
+
+separatedByTokenType and joinBackTokens(separatedByTokenType[-1]) is returned. joinBackTokens 'untokenizes' the tokens, that is joins things up and makes a list of strings, where each string is a cleaned tweet
+separatedByTokenType after spellcheck is: [[[('hello bye', '#', 0.765)], [('spe ling mistake', '#', 0.8214)], []],    [[], [(':)', 'E', 0.9852)], []],    [[('#helloBye', '#', 0.765), ('there', 'R', 0.5307)], [('an', 'D', 0.7642), (':)', 'E', 0.9852), ('#spelingMistake', '#', 0.8214)], [('world', '^', 0.3878), ('world', '^', 0.4131)]]]
+joinBackTokens gives: ['#helloBye there', 'an :) #spelingMistake', 'world world']
+'''
+
+
+def normTweet1(tweets, ops = [0,1], retain = 1, separateTokens = ['#', 'E']): 
     ignoreTags = ['E', ',', 'U', '&', '^', '!', '#']   #do not do anything if its a emoticon or punctuation or URL('U') or hashtag('#') or or abbreviations like lol ('!')
     threshold = 0.7  #only clean words to which high confidence POS is given. This stops the cleaner from replacing '@' with 'a' etc
     tokenizedTweets = tokenizeCMUPython(tweets)
-    #print tokenizedTweets
     f = [wordReplace, spellCorrectTokenizedTweets]
+
+    #separatedByTokenType has 3 parts say, eg: [hashtagsInTweetsText, emoticonInTweetsText, restInTweetsText]
+    separatedByTokenType = getSpecialTokens(tokenizedTweets, f, ops, separateTokens, retain)  #get hashtags and emoticons out of each tweet.     #retain=1 argument makes the last bin retain the tokenTypes that are already separated out in the first bins
+    
+    #do cleaning/spell check in last element of separatedByTokenType (which contains the bulk of the words)
     for op in ops:
-        tokenizedTweets = f[op](tokenizedTweets, ignoreTags, threshold)
-    #tokenizedTweets = wordReplace(tokenizedTweets, ignoreTags, threshold)
-    #tokenizedTweets = spellCorrectTokenizedTweets(tokenizedTweets, ignoreTags, threshold)
-    return [tokenizedTweets, joinBackTokens(tokenizedTweets)]
+        separatedByTokenType[-1] = f[op](separatedByTokenType[-1], ignoreTags, threshold)  #wordReplace(tokenizedTweets, ignoreTags, threshold) and spellCorrectTokenizedTweets(tokenizedTweets, ignoreTags, threshold)
+    return [separatedByTokenType, joinBackTokens(separatedByTokenType[-1])]
+    
+
+def getSpecialTokens(tokenizedTweets, funcs, ops, tokenTypes = ['#', 'E'], retain = 0):  #retain=1 argument makes the last bin retain the tokenTypes that are already separated out in the first bins
+    retval = [[[] for j in xrange(0, len(tokenizedTweets))] for i in xrange(0,len(tokenTypes)+1)]  #extra 1 for the 'not present in tokenTypes'
+    for tokenizedTweetIdx in xrange(0, len(tokenizedTweets)):
+        for word in tokenizedTweets[tokenizedTweetIdx]:
+            flag = 0
+            try:
+                idx = tokenTypes.index(word[1])
+            except:
+                idx = len(tokenTypes) #the last slot is the catch-all slot
+                flag = 1
+            if word[1] == '#' and '#' in tokenTypes:  #the hashtag is separated out and processed by calling parseTagSingleWord() if word[1] is '#
+                t = (parseTagSingleWord(word[0]), word[1], word[2])
+            else:
+                t = word          
+            if retain == 1 and flag == 0:
+                retval[len(tokenTypes)][tokenizedTweetIdx].append(word)  #if retain == 1, then we retain the unprocessed '#'
+            retval[idx][tokenizedTweetIdx].append(t)
+    return retval
 
 
 def wordReplace(tokenizedTweets, ignoreTags, threshold):
@@ -23,8 +67,9 @@ def wordReplace(tokenizedTweets, ignoreTags, threshold):
     for tokenizedTweet in tokenizedTweets:
         for wordIdx in xrange(len(tokenizedTweet)):
             word = tokenizedTweet[wordIdx]  #its a tuple as defined by khanh (word, tag, prob)
-            if word[0] in dictKeys and (word[1] not in ignoreTags) and (word[2] > threshold):
-                tokenizedTweet[wordIdx] = (d1[word[0]], word[1], word[2]);
+            wordtxt = word[0].lower()
+            if wordtxt in dictKeys and ((word[1] not in ignoreTags) and (word[2] > threshold) or (word[1] in ignoreTags and word[2] < threshold)):
+                tokenizedTweet[wordIdx] = (d1[wordtxt], word[1], word[2]);
     return tokenizedTweets
 
 
@@ -57,3 +102,4 @@ def tokenizeCMUPython(tweets):
     if success:
         print "Tokenizing..."
         return runtagger_parse(tweets, run_tagger_cmd=RUN_TAGGER_CMD)
+
---
+++ utilities/wordSeparator.py
@@ -0,0 +1,82 @@
+#http://stackoverflow.com/questions/20516100/term-split-by-hashtag-of-multiple-words
+#separates out words that are not space separated. Takes hints from caps, follows a greedy strategy
+
+def InitializeWords(wordlist):
+    content = None
+    with open(wordlist) as f:
+        content = f.readlines()
+    return [word.rstrip('\n') for word in content]
+
+def insertDashBeforeCaps(term):
+    #insert - before Caps letters if they dont have caps ahead of them. so awesomeDay -> awesome-Day. but IBM -> IBM
+    #Also if there is a bunch of caps, insert a - before the last in the bunch. eg helloWorldIBMRocks -> hello-World-IBM-Rocks   (note wont work for )  helloWorldIBMrocks
+    newterm = ''; capsRun = 0
+    for idx in xrange(0, len(term)):
+        if term[idx].isupper():
+            capsRun = capsRun + 1
+            if idx != 0:  #if the first letter is caps, dont add caps
+                if term[idx-1].islower():
+                    newterm += '-'
+                if term[idx-1].isupper() and capsRun > 1:
+                    if idx != len(term) - 1: #if the caps letter isnt the last letter
+                        if term[idx+1].islower():
+                            newterm += '-'
+        else:
+            capsRun = 0
+        newterm += term[idx]
+    return newterm
+
+def parseTagSingleWord(term):
+    wordlisttxt = 'utilities/englishWords.txt' # A file containing common english words  #assumes we are running from the top directory (schizophreniaThroughTweets)
+    wordlist = InitializeWords(wordlisttxt)
+    words = []
+    # Remove hashtag, 
+    if term[0] == '#':
+        term = term[1:]
+    if term.isupper():  #everything is caps, so no information comes from caps. so lowercasing everything
+        term = term.lower()
+    term = insertDashBeforeCaps(term)
+    #split by dash
+    tags = term.split('-')
+    for tag in tags:
+        if tag.isupper():  #if all letters are caps, just retain the word
+            if tag.lower() in wordlist: #lowercase the word if it is present in the word list
+                word = tag.lower()
+            else:
+                word = tag
+        else:
+            word = FindWord(tag, wordlist) 
+        while word != None and len(tag) > 0:
+            words += [word]
+            if len(tag) == len(word): # Special case for when eating rest of word
+                break
+            tag = tag[len(word):]
+            word = FindWord(tag, wordlist)
+    return " ".join(words)
+
+def FindWord(token, wordlist):  #longest match 
+    i = len(token) + 1
+    while i > 1:
+        i -= 1
+        if token[:i].lower() in wordlist:
+            return token[:i].lower()
+    return token #if no match is found just return the whole thing
+
+def parseTag(term):  #term can be a list or a single string
+    if type(term) is str:
+        term = [term]  #convert to list form (singleton list)
+    return [parseTagSingleWord(t) for t in term]
+
+
+#TESTING HASHTAG SPLITTER
+'''
+sentence = "big #awesome-dayofmylife because #iamgreat"
+print parseTag('#iamgreat')
+print parseTag(['#HelloWorldIBMrocks', '#HelloWorldIBMRocks', 'helloWorldFLYYouFool', 'wywyywywyw'])
+print insertDashBeforeCaps('helloWorld') #hello-World
+print insertDashBeforeCaps('helloWorldIBM') #hello-World-IBM
+print insertDashBeforeCaps('helloWorldIBMwow')  #hello-World-IB-Mwow
+print insertDashBeforeCaps('HelloWorldIBMWow') #Hello-World-IBM-Wow
+print insertDashBeforeCaps('HelloWorld-IBMWow') #Hello-World-IBM-Wow
+print insertDashBeforeCaps('IBMrocks')  #IB-Mrocks
+'''
