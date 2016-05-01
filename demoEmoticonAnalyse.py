2e62a01e71316df38e9f95122e6cd8bfb48137b7
ssarkar2 <supersarkar@gmail.com>
2016-05-01 04:33:26 -0400

    Modifying emoticon analysis
    From this analysis, we do not find discriminative features

--- demoEmoticonAnalyse.py
+++ demoEmoticonAnalyse.py
@@ -1,5 +1,5 @@
 import cPickle as pickle
-import os, timeit
+import os, timeit, csv
 from utilities.normalizeTweets import *
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
@@ -18,15 +18,15 @@ def getData(): #returns 2 lists of strings for the 2 groups
             allControlTweets, allSchTweets = pickle.load(f)
     else:
         #read lines of csv
-        allControl = readCSV(csvFileLoc, {'condition':'control'})  
-        allSch = readCSV(csvFileLoc, {'condition':'schizophrenia'})  
+        allControl = readCSV(csvFileLoc, {'condition':'control'})
+        allSch = readCSV(csvFileLoc, {'condition':'schizophrenia'})
 
         #get tweets by read files.
         start = timeit.default_timer()
         allControlTweets = getTweetsForGroup(allControl, control_folder_path, sch_folder_path, fields = ['text'])  #read the files and get the tweet (only the text as specified in 'field')
         allSchTweets = getTweetsForGroup(allSch, control_folder_path, sch_folder_path, fields = ['text'])
-        allControlTweets = getFieldFromGroup(allControlTweets, field = 'text')
-        allSchTweets = getFieldFromGroup(allSchTweets, field = 'text')
+        allControlTweets = getFieldDictFromGroupPerUser(allControlTweets, field = 'text')
+        allSchTweets = getFieldDictFromGroupPerUser(allSchTweets, field = 'text')
         print 'got tweets', timeit.default_timer() - start
 
         with open(picklefile, 'w') as f:
@@ -38,8 +38,13 @@ def getEmoticonAnnotation(emoticonAnnotatedFile):
     return {i[0]:float(i[3]) for i in t if len(i) == 4}
 
 def getEmoticonFeature(emoList, emoticonScoreDict):  #emoList = [[':)', ':P'], [], [':(']]. empty lists denote those tweets did not have emoticons in their text
-    emoScore = [[emoticonScoreDict.get(emo,0) for emo in tw] for tw in emoList]      
-    return [((np.mean(score), np.var(score), len(score)), (0.,0.,0))[len(score)==0] for score in emoScore]  #list of 3-tuples. each tuple is the emoticon feature for that tweet (mean, variance, number of emoticons)
+    emoScore = {user:[emoticonScoreDict.get(emo[0],0) for emo in emoList[user]] for user in emoList}
+    retval = {}
+    for user in emoScore:
+        t = emoScore[user]
+        retval[user] = ((np.mean(t), np.var(t), len(t)), (0.,0.,0))[len(t)==0]
+    return retval
+    #return {user:((np.mean(emoScore[user]), np.var(emoScore[user]), len(emoScore[user])), (0.,0.,0))[len(emoScore[user])==0] for user in emoScore}  #list of 3-tuples. each tuple is the emoticon feature for that tweet (mean, variance, number of emoticons)
 
 
 
@@ -52,16 +57,35 @@ if os.path.isfile(picklefile):
         normControl, normSch = pickle.load(f)
 else:
     start = timeit.default_timer()
-    normControl = normTweet1(allControlTweets, ops = [], retain = 1, separateTokens = ['E'])  #being lazy here. the emoticons AND the text is also returned by normTweet1. We dont need the text for this exercise
+    #normControl = {user:normTweet1(allControlTweets[user], ops = [], retain = 1, separateTokens = ['E'])[0][0] for user in allControlTweets}  #being lazy here. the emoticons AND the text is also returned by normTweet1. We dont need the text for this exercise
+    normControl = {}
+    C = 0
+    for user in allControlTweets:
+        print 'CTRL', user, C; C+=1
+        normControl[user] = normTweet1(allControlTweets[user], ops = [], retain = 1, separateTokens = ['E'])[0][0]
     print 'got normControl', timeit.default_timer() - start  #387s
     start = timeit.default_timer()
-    normSch = normTweet1(allSchTweets, ops = [], retain = 1, separateTokens = ['E'])
-    print 'got normSch', timeit.default_timer() - start  #384s (why so less compared to normControl? CHECK
+    #normSch = {user:normTweet1(allSchTweets[user], ops = [], retain = 1, separateTokens = ['E'])[0][0] for user in allSchTweets}
+    normSch = {}
+    C=0
+    for user in allSchTweets:
+        print 'SCH', user,C; C+=1
+        normSch[user] = normTweet1(allSchTweets[user], ops = [], retain = 1, separateTokens = ['E'])[0][0]
+    print 'got normSch', timeit.default_timer() - start  #384s
     with open(picklefile, 'w') as f:
         pickle.dump([normControl, normSch], f)
 
-controlEmo = [[emo[0] for emo in tw] for tw in normControl[0][0]]  #normControl[0][0] is of the form [[(':)', 'E', 0.9), (':P', 'E', 0.99)],  []]
-schEmo = [[emo[0] for emo in tw] for tw in normSch[0][0]]
+'''
+controlEmo = {}
+for user in normControl:
+    print user
+    for emo in normControl[user]:
+        print emo
+    controlEmo[user] = [emo[0] for emo in normControl[user] i]
+    '''
+
+controlEmo = {user:[emo[0] for emo in normControl[user] if emo != []] for user in normControl}  #key is username, value is of the form [[(':)', 'E', 0.9), (':P', 'E', 0.99)],  []]
+schEmo =     {user:[emo[0] for emo in normSch[user] if emo != []] for user in normSch}
 
 
 emoticonScoreDict = getEmoticonAnnotation('utilities/emoticonList_annotated.txt')
@@ -72,8 +96,8 @@ schFeat = getEmoticonFeature(schEmo, emoticonScoreDict)
 #3d plots
 #fig = plt.figure()
 #ax = fig.add_subplot(111, projection='3d')
-xc = [i[0] for i in controlFeat]; yc = [i[1] for i in controlFeat]; zc = [i[2] for i in controlFeat]
-xs = [i[0] for i in schFeat]; ys = [i[1] for i in schFeat]; zs = [i[2] for i in schFeat]
+xc = {user:controlFeat[user][0] for user in controlFeat}; yc = {user:controlFeat[user][1] for user in controlFeat}; zc = {user:controlFeat[user][2] for user in controlFeat}
+xs = {user:schFeat[user][0] for user in schFeat}; ys = {user:schFeat[user][1] for user in schFeat}; zs = {user:schFeat[user][2] for user in schFeat}
 
 #ax.scatter(xs, ys, zs, c='r', marker='o')
 #ax.scatter(xc, yc, zc, c='b', marker='^')
@@ -86,16 +110,29 @@ xs = [i[0] for i in schFeat]; ys = [i[1] for i in schFeat]; zs = [i[2] for i in 
 #plt.plot(xs, zs, 'r^', xc, zc, 'bs'); plt.show()
 #plt.plot(zs, ys, 'r^', zc, yc, 'bs'); plt.show()
 
-sio.savemat('emoticonAnalyse.mat', {'xc':xc, 'yc':yc, 'zc':zc, 'xs':xs, 'ys':ys, 'zs':zs})
+#sio.savemat('emoticonAnalyse.mat', {'xc':xc, 'yc':yc, 'zc':zc, 'xs':xs, 'ys':ys, 'zs':zs})
 
-print 'mean of ft 1', np.mean(xc), np.mean(xs)
-print 'mean of ft 2', np.mean(yc), np.mean(ys)
-print 'mean of ft 3', np.mean(zc), np.mean(zc)
+with open('emoticonFeaturesCtrl.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for user in controlFeat:
+        writer.writerow([user, xc[user], yc[user], zc[user]])
+
+
+with open('emoticonFeaturesSch.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    for user in schFeat:
+        writer.writerow([user, xs[user], ys[user], zs[user]])
+
+
+
+print 'mean of ft 1', np.mean(xc.values()), np.mean(xs.values())
+print 'mean of ft 2', np.mean(yc.values()), np.mean(ys.values())
+print 'mean of ft 3', np.mean(zc.values()), np.mean(zc.values())
 print '# of tweets in each group', len(controlFeat), len(schFeat)
 
-print 't test feat 1', ttest_ind(xc, xs)
-print 't test feat 2', ttest_ind(yc, ys)
-print 't test feat 3', ttest_ind(zc, zs)
+print 't test feat 1', ttest_ind(xc.values(), xs.values())
+print 't test feat 2', ttest_ind(yc.values(), ys.values())
+print 't test feat 3', ttest_ind(zc.values(), zs.values())
 
 '''
 mean of ft 1 0.044251820544 0.0407166580237
@@ -105,4 +142,3 @@ mean of ft 3 0.120600806558 0.120600806558
 t test feat 1 Ttest_indResult(statistic=6.4992050181153491, pvalue=8.0781157244368766e-11)
 t test feat 2 Ttest_indResult(statistic=14.252762789068353, pvalue=4.351486585757677e-46)
 t test feat 3 Ttest_indResult(statistic=23.042523842720197, pvalue=1.8687147480576763e-117)
-'''
--- emoticonAnalyse.m
+++ emoticonAnalyse.m
@@ -1,69 +1,38 @@
-function untitled()
-load('emoticonAnalyse.mat')
+function emoticonAnalyse()
 close all;
-zs = double(zs); zc = double(zc);
-s = [xs; ys;zs];
-c = [xc; yc;zc];
+c = xlsread('emoticonFeaturesCtrl.csv');
+s  = xlsread('emoticonFeaturesSch.csv');
+plot(s(:,1), s(:,2), 'ro'); hold on
+plot(c(:,1), c(:,2), 'bo'); hold on; grid on
 
-% % % length(find(sum(abs(s)) == 0))  %488879
-% % % length(find(sum(abs(c)) == 0))  %477206
-% % % {size(s,2), size(c,2)}  %[529257]    [530154]
-% % % s(:, find(sum(abs(s)) == 0)) = [] ;  %removing (0,0,0) points from data
-% % % c(:, find(sum(abs(c)) == 0)) = [] ;
-% % % {size(s,2), size(c,2)}  %[40378]    [52948]
+%blue is the 1st column, red is the second
+figure;
+hist([s(:,1), c(:,1)], 20); grid on %we see more 'mean rhyming' for the blue
+title('Histogram of mean emoticon scores')
+xlabel('mean emoticon score')
+ylabel('number of occurances')
+[a,b] = ttest2(s(:,1), c(:,1))  %p val = 0.8271
 
-xs = s(1,:); ys = s(2,:); zs = s(3,:);
-xc = c(1,:); yc = c(2,:); zc = c(3,:);
+figure;
+hist([s(:,2), c(:,2)], 20); grid on
+title('Histogram of variance of emoticon scores')
+xlabel('emoticon score variance')
+ylabel('number of occurances')
+[a,b] = ttest2(s(:,2), c(:,2))  %p val = 0.6045
 
-
-% [h,p] = ttest2(xs,xc)
-% [h,p] = ttest2(ys,yc)
-% [h,p] = ttest2(zs,zc)
-% 
-% hist(xs, 100); waitforbuttonpress; hist(xc, 100); waitforbuttonpress;
-% hist(ys, 100); waitforbuttonpress; hist(yc, 100); waitforbuttonpress;
-% hist(zs, 100); waitforbuttonpress; hist(zc, 100); waitforbuttonpress;
-
-mean(xs)
-mean(xc)
-mean(ys)
-mean(yc)
-mean(zs)
-mean(zc)
-
-plot(xs,ys, 'ro'); hold on; plot(xc,yc, 'b^');
-figure; rotate3d on; scatter3(xs,ys,zs,'ro'); hold on; scatter3(xc,yc,zc,'b^');
-pcaStuff(c,s)
-end
+figure;
+hist([s(:,3), c(:,3)], 20); grid on
+title('Histogram of number of emoticons')
+xlabel('number of emoticons')
+ylabel('number of occurances')
+[a,b] = ttest2(s(:,3), c(:,3))  %p val = 0.1568
 
 
 
-
-
-function pcaStuff(c,s)
-'pcastuff'
-slen = size(s,2);
-clen = size(c,2);
-[coeff score latent] = pca([s';c']);
-scoreS = score(1:slen,:);
-scoreC = score(slen+1:end,:);
-
-spca1 = scoreS(:,1); spca2 = scoreS(:,2); spca3 = scoreS(:,3);
-cpca1 = scoreC(:,1); cpca2 = scoreC(:,2); cpca3 = scoreC(:,3);
-
-
-figure;plot(spca1,spca2, 'ro'); hold on; plot(cpca1,cpca2, 'b^'); grid on;
-nbins = [50,50];
-figure; grid on; hist3([spca1 spca2], nbins);
-figure; grid on; hist3([cpca1 cpca2], nbins);
-
-
-figure; hist(spca1, 100, 'r'); figure; hist(cpca1, 100, 'b');
-
-
-
-[h,p] = ttest2(spca1,cpca1)
-[h,p] = ttest2(spca2,cpca2)
-[h,p] = ttest2(spca3,cpca3)
-
+mean(s(:,1))
+mean(c(:,1))
+mean(s(:,2))
+mean(c(:,2))
+mean(s(:,3))
+mean(c(:,3))
 end
